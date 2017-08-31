78852617fe08a528d0f1666a6817647cfe241ab4
Kartikay Gupta <kartikayg@gmail.com>
2017-08-31 15:15:57 -0400

    committing the basic structure for the app. it will probably update as I progress.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 // @flow
 import express from 'express';
-import debug from 'debug';
 
 const app = express();
-const log = debug('app:log');
 
 app.listen(4040, () => {
-  log('app started');
 });
