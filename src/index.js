e0e656b0fb199b2af5ab2be820f0db21030b8de7
Kartikay Gupta <kartikayg@gmail.com>
2017-09-15 13:17:11 -0400

    structure changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,1 @@
 // @flow
-import express from 'express';
-
-const app = express();
-
-app.listen(4040, () => {
-});
---
+++ src/server/index.js
@@ -0,0 +1,20 @@
+// load express
+
+// parse body params and attache them to req.body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.use(cookieParser());
+app.use(compress());
+app.use(methodOverride());
+
+// secure apps by setting various HTTP headers
+app.use(helmet());
+
+// enable CORS - Cross Origin Resource Sharing
+app.use(cors());
+
+
+// load routes
+
+// unhandled exceptions
