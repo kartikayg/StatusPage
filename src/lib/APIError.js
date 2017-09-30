add6860c4e6fe1a570e6a725b8aa5823bc6a29fc
Kartikay Gupta <kartikayg@gmail.com>
2017-09-30 15:41:16 +0530

    adding a custom error class

---
+++ src/lib/APIError.js
@@ -0,0 +1,23 @@
+import httpStatus from 'http-status';
+
+/**
+ * Class representing an API error.
+ * @extends Error
+ */
+class APIError extends Error {
+  
+  /**
+   * Creates an API error.
+   * @param {string} message - Error message.
+   * @param {number} status - HTTP status code of error.
+   */
+  constructor(message, status = httpStatus.INTERNAL_SERVER_ERROR) {
+    super(message);
+    this.name = this.constructor.name;
+    this.message = message;
+    this.status = status;
+    Error.captureStackTrace(this, this.constructor.name);
+  }
+}
+
+export default APIError;
