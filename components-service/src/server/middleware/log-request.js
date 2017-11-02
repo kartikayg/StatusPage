92cefe581716e19469e92ddffa28369d03c8cb36
Kartikay Gupta <kartikayg@gmail.com>
2017-11-02 13:56:25 -0400

    testing log-request class

--- components-service/src/server/middleware/log-request.js
+++ components-service/src/server/middleware/log-request.js
@@ -1,6 +1,6 @@
 /**
  * @fileoverview Middleware to log all HTTP requests coming 
- * to the server
+ * to the server. This uses a combination of winston and morgan.
  */
 
 import morgan from 'morgan';
@@ -8,24 +8,30 @@ import winston from 'winston';
 
 /**
  * Returns the middleware to log all requests
- * @param {string} logWriter - Which log writer to use
- *   file or console
+ * @param {object} conf - Configuration for the logger
+ *   LOG_HTTP_REQUEST_WRITER - console OR file
+ *   LOG_HTTP_REQUEST_DIRNAME - file writer conf
+ *   LOG_HTTP_REQUEST_PREFIX  - file writer conf
+ * @param {boolean} immediate - Whether to log right away or
+ *  or at the end of the request. If immediate, some options
+ *  may not be logged.
+ * @return {function} express middleware
  */
-export default (conf = {}) => {
+export default (conf = {}, immediate = false) => {
 
-  const logger = new winston.Logger({
-    level: 'info'
-  });
+  const level = 'info';
+
+  const logger = new winston.Logger({ level });
+
+  const formatter = (options) => {
+    return options.message.trim();
+  };
 
   // based on the 
   switch (conf.LOG_HTTP_REQUEST_WRITER) {
 
     case 'console':
-      logger.add(winston.transports.Console, {
-        formatter: (options) => {
-          return options.message.trim();
-        }
-      });
+      logger.add(winston.transports.Console, { formatter });
       break;
 
     case 'file':
@@ -33,15 +39,12 @@ export default (conf = {}) => {
       require('winston-daily-rotate-file'); // eslint-disable-line global-require
 
       logger.add(winston.transports.DailyRotateFile, {
-        level: 'info',
         dirname: conf.LOG_HTTP_REQUEST_DIRNAME,
         filename: conf.LOG_HTTP_REQUEST_PREFIX || 'request',
         datePattern: '-yyyy-MM-dd.log',
         maxDays: 7,
         json: false,
-        formatter: (options) => {
-          return options.message.trim();
-        }
+        formatter
       });
       break;
 
@@ -50,12 +53,12 @@ export default (conf = {}) => {
   }
 
   function write(msg) {
-    logger.log('info', msg.trim());
+    logger.log(level, msg.trim());
   }
 
   // what to log
   const fmt = ':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms';
 
-  return morgan(fmt, { stream: { write } });
+  return morgan(fmt, { stream: { write }, immediate });
 
 };
