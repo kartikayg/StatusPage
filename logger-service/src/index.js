08385da410bd29232d44e6bb5d2aba58c7080811
Kartikay Gupta <kartikayg@gmail.com>
2018-01-10 10:41:29 -0500

    logger-service: updating index.js to match other microservices

--- logger-service/src/index.js
+++ logger-service/src/index.js
@@ -2,30 +2,26 @@
  * @fileoverview Entry point
  */
 
-import * as app from './app';
+import app from './app';
 
 // events on the process
 
-process.on('exit', (code) => {
-  console.log(`About to exit with code: ${code}`); // eslint-disable-line no-console
-  app.shutdown();
-});
-
 process.on('uncaughtException', (err) => {
   console.error(`Uncaught exception: ${err}`); // eslint-disable-line no-console
-  process.exit(1);
 });
 
 process.on('unhandledRejection', (reason) => {
   console.error(`Unhandled rejection: ${reason}`); // eslint-disable-line no-console
-  process.exit(1);
 });
 
 process.on('SIGTERM', () => {
-  process.exit(1);
+  app.shutdown();
+  process.exit(0);
 });
+
 process.on('SIGINT', () => {
-  process.exit(1);
+  app.shutdown();
+  process.exit(0);
 });
 
 // start microservice
@@ -34,3 +30,4 @@ app.start()
     console.error('Problem initializing the app', e); // eslint-disable-line no-console
     process.exit(1);
   });
+
