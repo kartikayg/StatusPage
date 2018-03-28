012a1052aa8f484ae9d3ee8b699cd82e58776cbc
Kartikay Gupta <kartikayg@gmail.com>
2018-03-28 15:48:27 -0400

    adding docs to invidual prod start script

--- logger-service/prod_start.sh
+++ logger-service/prod_start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+# Starting with a sleep so we can wait for rabbitmq to stand up.
 sleep 10
 node /app/dist/src/index.js
--- notification-service/prod_start.sh
+++ notification-service/prod_start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+# Starting with a sleep so we can wait for rabbitmq to stand up.
 sleep 15
 node /app/dist/src/index.js
