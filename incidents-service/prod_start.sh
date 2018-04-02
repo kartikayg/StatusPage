e27809cc5aab2add7ff98eb654d4bed738a4f0da
Kartikay Gupta <kartikayg@gmail.com>
2018-04-02 12:11:30 -0400

    delay time changes in prod_start script and minor updates in docker-compose

--- incidents-service/prod_start.sh
+++ incidents-service/prod_start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Starting with a sleep so we can wait for rabbitmq to stand up.
-sleep 15
+sleep 5
 node /app/dist/src/index.js
--- logger-service/prod_start.sh
+++ logger-service/prod_start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Starting with a sleep so we can wait for rabbitmq to stand up.
-sleep 10
+sleep 3
 node /app/dist/src/index.js
